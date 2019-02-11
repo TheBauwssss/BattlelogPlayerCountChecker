f95de436409b98659a5b0db332ee0144a24ce29f
TheBauwssss <TheBauwssss@users.noreply.github.com>
2019-02-11 02:39:24 +0100

    Error handler for Firefox, for some reason jQuery request fails with status code 0 error

--- battlelog.js
+++ battlelog.js
@@ -1,5 +1,15 @@
-var serverGuid = "";
 var URL_BASE = "https://keeper.battlelog.com/snapshot/";
+var debug = true;
+
+//Setup jQuery AJAX error reporting
+$.ajaxSetup({
+  error: function(xhr, status, error) {
+    console.log("An AJAX error occured: " + status + "\nError: " + error);
+	console.log("XHR: " + xhr);
+  }
+});
+
+var serverGuid = "";
 
 var regex = new RegExp(/[a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12}/);
 
@@ -23,6 +33,15 @@ if (found != null && found.length > 0)
 	setInterval(processSearchResults, 5000);
 }
 
+function parse(data)
+{
+	//Fix for Firefox: https://stackoverflow.com/questions/15937416/jquery-get-not-working-in-firefox-and-ie
+	if (typeof data === "string"){
+		return JSON.parse(data);
+	}
+	
+	return data;
+}
 
 function processSearchResults()
 {
@@ -98,51 +117,71 @@ function processSearchResults()
 
 function processServerPage()
 {
-	$.get(URL_BASE + serverGuid, function( data ) {
-	console.log(data);
+	var jsonUrl = URL_BASE + serverGuid;
 	
-	if (data.snapshot.status == "SUCCESS")
-	{
-		var actualPlayerCount = 0;
-		var maxPlayers = data.snapshot.maxPlayers;
-		
-		for(var team_id in data.snapshot.teamInfo)
-		{
-			for(var player_id in data.snapshot.teamInfo[team_id].players)
+	console.log("Sending GET request for JSON data with url: " + jsonUrl);
+	
+	$.ajax({
+		async: false,
+		url: jsonUrl,
+		type: 'GET',
+		dataType: 'json',
+		success: function( data ) {
+			data = parse(data); //Make sure data is parsed correctly for Firefox and IE
+			
+			console.log(data);
+			
+			if (data.snapshot.status == "SUCCESS")
 			{
-				//console.log("Team ID " + team_id + " - Player ID " + player_id);
-				actualPlayerCount++;
+				var actualPlayerCount = 0;
+				var maxPlayers = data.snapshot.maxPlayers;
+				
+				for(var team_id in data.snapshot.teamInfo)
+				{
+					for(var player_id in data.snapshot.teamInfo[team_id].players)
+					{
+						//console.log("Team ID " + team_id + " - Player ID " + player_id);
+						actualPlayerCount++;
+					}
+				}
+				
+				console.log("Server actual player count: " + actualPlayerCount);
+				
+				var playerCountObject = $('#server-page-info > div:nth-child(2) > div:nth-child(1) > section > h5')[0];
+				
+				var content = playerCountObject;
+				
+				var number = $(content).html().trim().split(' / '); //really ugly but I want this done, I'm tired
+				
+				var reportedPlayerCount = parseInt(number);
+				
+				console.log("Server reported player count: " + reportedPlayerCount);
+				
+				if (actualPlayerCount != reportedPlayerCount)
+				{
+					console.log("Server is SPOOFING player count. Adding warning symbol.");
+					
+					var html_obj = "<div id='spoof-warning'></div><h5 id='spoof-text'>" + actualPlayerCount + " / " + maxPlayers + "</h5><h5>SPOOFED</h5>";
+					
+					$(playerCountObject).replaceWith(html_obj);
+				} else {
+					console.log("Server is not spoofing player count. Adding ok/verified symbol.");
+					
+					var html_obj = "<div id='spoof-ok'></div><h5 id='spoof-text'>" + actualPlayerCount + " / " + maxPlayers + "</h5><h5>VERIFIED</h5>";
+					
+					$(playerCountObject).replaceWith(html_obj);
+				}
+			} else 
+			{
+				console.log("Unable to query for snapshot data. data.snapshot.status field not set to 'SUCCESS'. Actual data='" + data + "'");
 			}
-		}
+	},
+	// error: function(data) { 
+		// data = parse(data); //Make sure data is parsed correctly for Firefox and IE
+		// console.log("ERROR: " + data); 
+	// }
+	});
 		
-		console.log("Server actual player count: " + actualPlayerCount);
-		
-		var playerCountObject = $('#server-page-info > div:nth-child(2) > div:nth-child(1) > section > h5')[0];
-		
-		var content = playerCountObject;
-		
-		var number = $(content).html().trim().split(' / '); //really ugly but I want this done, I'm tired
-		
-		var reportedPlayerCount = parseInt(number);
-		
-		console.log("Server reported player count: " + reportedPlayerCount);
-		
-		if (actualPlayerCount != reportedPlayerCount)
-		{
-			console.log("Server is SPOOFING player count. Adding warning symbol.");
-			
-			var html_obj = "<div id='spoof-warning'></div><h5 id='spoof-text'>" + actualPlayerCount + " / " + maxPlayers + "</h5><h5>SPOOFED</h5>";
-			
-			$(playerCountObject).replaceWith(html_obj);
-		} else {
-			console.log("Server is not spoofing player count. Adding ok/verified symbol.");
-			
-			var html_obj = "<div id='spoof-ok'></div><h5 id='spoof-text'>" + actualPlayerCount + " / " + maxPlayers + "</h5><h5>VERIFIED</h5>";
-			
-			$(playerCountObject).replaceWith(html_obj);
-		}
-	}
-  
-});
+	console.log("Server page processed. Exiting script.");
 }
 
